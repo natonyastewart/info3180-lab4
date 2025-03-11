e8edbf226049af365d4d9c92ef71fa81345040cf
natonyastewart <natonyastewart15@gmail.com>
2025-03-10 19:32:41 -0500

    Exercise 2

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import db
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class UserProfile(db.Model, UserMixin):
@@ -13,6 +14,19 @@ class UserProfile(db.Model, UserMixin):
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
     username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(128), nullable=False) #password
+
+    def __init__(self, first_name, last_name, username, password):
+        self.fist_name = first_name
+        self.last_name = last_name
+        self.username = username 
+        self.new_password(password)
+
+    def new_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def password_check(self, password):
+        return check_password_hash(self.password, password)
 
     def is_authenticated(self):
         return True
@@ -27,4 +41,4 @@ class UserProfile(db.Model, UserMixin):
         return str(self.id)  # python 3 support
 
     def __repr__(self):
-        return '<User %r>' % (self.username)
+        return f'<User {self.username}>'
